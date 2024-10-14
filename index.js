395efeb5ed1d8c06ce456b084e0b10318179eb97
Iykelistic <ikechukwujames902@gmail.com>
2024-10-14 16:34:52 +0100

    added a scroll effect

---
+++ index.js
@@ -0,0 +1,6 @@
+document.querySelector(".scroll").addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth", // This provides a smooth scrolling effect
+  });
+});
